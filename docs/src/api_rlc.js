738b42eaa16f3d99e9b7fd06221a44b465e25359
Unknown <svetlana.ovchinnikova@helsinki.fi>
2019-01-03 18:18:13 +0100

    Few changes in toc structure

--- docs/src/api_rlc.js
+++ docs/src/api_rlc.js
@@ -1,5 +1,5 @@
-var nav = d3.select(".tocContainer")
-	.append("ul");
+var nav = d3.select(".tocContainer");
+nav.append("ul");
 
 d3.select("#content")
 	.selectAll("h1")
@@ -7,7 +7,7 @@ d3.select("#content")
 			var hTitle = d3.select(this).attr("id");
 			d3.select(this.nextSibling.nextSibling)
 				.selectAll("li").each(function() {
-					nav
+					nav.select("ul")
 						.append("li")
 							.append("a")
 								.attr("id", d3.select(this).select("code").text())
@@ -15,3 +15,16 @@ d3.select("#content")
 								.html(d3.select(this).html());
 				})
 		})
+
+var navOffset = nav.node().offsetTop;
+
+window.onscroll = function() {
+	if(window.pageYOffset >= navOffset)
+		nav
+			.style("position", "fixed")
+			.style("top", "0")
+	else 
+		nav
+			.style("position", "absolute")
+			.style("top", undefined);
+}
