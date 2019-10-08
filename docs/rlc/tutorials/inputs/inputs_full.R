f86a5590b8ea077979e42ec02311a30ef6bd7663
Svetlana <s.ovchinnikova@zmbh.uni-heidelberg.de>
2019-10-08 13:53:30 +0200

    Example for input tutorial finished

--- docs/rlc/tutorials/inputs/inputs_full.R
+++ docs/rlc/tutorials/inputs/inputs_full.R
@@ -11,24 +11,30 @@ X <- cbmc@assays$RNA@data
 #rm(cbmc)
 
 #subset
-cells <- sample(length(clusters), 2500)
+cells <- sample(length(clusters), 4000)
 
 clusters <- clusters[cells]
 pca <- pca[cells, ]
 X <- X[, cells]
 
-um <- umap(pca[, 1:15])
+npca <- 15
+negRate <- 5
+spread <- 1
+
+um <- umap(pca[, 1:npca], negative_sample_rate = negRate, spread = spread)
 useClusters <- levels(clusters)
+inclCells <- rep(T, ncol(X))
 
 selGene <- "ADC"
 
 openPage(useViewer = FALSE, layout = "table1x2")
 
-lc_scatter(dat(opacity = ifelse(clusters %in% useClusters, 1, 0.05),
+lc_scatter(dat(opacity = ifelse(clusters[inclCells] %in% useClusters, 0.8, 0.1),
                colourLegendTitle = selGene,
-               colourValue = X[selGene, ]),
-           x = um[, 1], y = um[, 2], size = 1, 
-           palette = RColorBrewer::brewer.pal( 9, "YlOrRd" ), 
+               colourValue = X[selGene, inclCells], 
+               x = um[, 1], y = um[, 2]),
+           size = 2, 
+           palette = rje::cubeHelix(n = 11)[9:1], 
            place = "A1")
 
 lc_input(type = "checkbox", 
@@ -62,8 +68,23 @@ lc_html(content = "<p style='color: red; display: none'>There is no such gene</p
 
 lc_input(type = "range",
          label = c("Number of PCs", "Spread", "Negative rate"),
+         width = 250,
          value = c(15, 1, 5),
          min = c(2, 0.01, 0.5),
          max = c(50, 5, 20),
          step = c(1, 0.01, 0.5),
-         on_change = function(value) print(value))
+         on_change = function(value) {
+           npca <<- value[1]
+           spread <<- value[2]
+           negRate <<- value[3]
+         })
+
+lc_input(type = "button",
+         label = "Recalculate", 
+         on_click = function(value) {
+           print("Recalculation started. Please, wait...")
+           inclCells <<- clusters %in% useClusters
+           um <<- umap(pca[inclCells, 1:npca], negative_sample_rate = negRate, spread = spread)
+           print("Recalculation finished")
+           updateCharts("A1")
+         })
